dca95ba9335c1767fd32cf06c7202bfc3b3f5db1
jeremymarch <jmarch@gradcenter.cuny.edu>
2026-02-26 01:47:52 -0500

    fix font delivery issue; scrollbar styling for windows; focus styling for ubuntu

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/static/philolog/react_build/",
+  base: "/static/",
   plugins: [react()],
   build: {
     emptyOutDir: true,
