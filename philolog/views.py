0f73ae372f1bca38ab4f9effd2bd1c5831ac579b
jeremymarch <jmarch@gradcenter.cuny.edu>
2026-02-27 23:00:28 -0500

    increaet page_size of fetchdata search query

--- philolog/views.py
+++ philolog/views.py
@@ -87,7 +87,7 @@ def query_words(word_prefix, lex, page, page_size):
 
 def get_words(request):
     """Returns page_size words above and below given string prefix."""
-    page_size = 50
+    page_size = 100
     query_data = json.loads(request.GET["query"])
     page = int(request.GET["page"])
 
